353f901de347860253c0ee1d85f890ffa0e9a52a
Brent Heeringa <heeringa@mindburner.io>
2017-09-26 12:04:51 -0400

    rebuilding site Tue Sep 26 12:04:51 EDT 2017

--- js/smart-app-banner.js
+++ js/smart-app-banner.js
@@ -44,14 +44,12 @@ $(document).ready(function() {
           });
             
           $('.smart-banner').stop().animate({top:0},300);
-          /*$('.navbar').affix({
-              offset: {
-                top: 82
-              }
-            }); */
+          $('.navbar').affix({
+              offset: {top: 82}
+            }); 
 
           //$('.container').css({marginTop:78});
-          $('#page-top').animate({marginTop:origHtmlMargin+78},300); 
+          $('html').animate({marginTop:origHtmlMargin+78},300); 
         }
       }); 
     }
