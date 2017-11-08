e54a2c04125807f7145e28e1ff4d4262e2b234ba
Brent Heeringa <heeringa@mindburner.io>
2017-11-07 22:33:40 -0500

    rebuilding site Tue Nov  7 22:33:40 EST 2017

--- js/agency.js
+++ js/agency.js
@@ -1,30 +1,3 @@
-// Smooth scrolling via animate()
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash && window.location.pathname == "/") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
-    }
-  });
-});
-
-// Navigation change on scroll
-$(document).ready(function(){
-  var maxOffset = 300;
-  $(window).scroll(function() {
-    if ($(window).scrollTop() >= maxOffset) {
-      $('.navbar-default').addClass('navbar-shrink');
-    }
-    else {
-      $('.navbar-default').removeClass('navbar-shrink');
-    }
-  });
-});
 
 // only display mobile icons when non-safari
 $(document).ready(function(){
---
+++ js/nav.js
@@ -0,0 +1,37 @@
+// Smooth scrolling via animate()
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash && window.location.pathname == "/") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+// Navigation change on scroll
+$(document).ready(function(){
+  var maxOffset = 300;
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= maxOffset) {
+      $('.navbar-default').addClass('navbar-shrink');
+    }
+    else {
+      $('.navbar-default').removeClass('navbar-shrink');
+    }
+  });
+});
+
+$(document).ready(function(){
+  var maxOffset = 300;
+  if ($(window).scrollTop() >= maxOffset) {
+    $('.navbar-default').addClass('navbar-shrink');
+  }
+  else {
+    $('.navbar-default').removeClass('navbar-shrink');
+  }
+});
