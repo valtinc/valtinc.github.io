4152149e227e99b964732a83586921c6878e896b
Brent Heeringa <heeringa@mindburner.io>
2018-04-05 12:54:13 -0400

    rebuilding site Thu Apr  5 12:54:13 EDT 2018

--- js/agency.js
+++ js/agency.js
@@ -1,15 +1,4 @@
 
-// only display mobile icons when non-safari
-/*$(document).ready(function(){
-  var ua = window.navigator.userAgent;
-  var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
-  var webkit = !!ua.match(/WebKit/i);
-  var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
-  if (iOSSafari) {
-    document.getElementById("mobile-icons").style.display = "none";
-  }
-});
-*/
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
